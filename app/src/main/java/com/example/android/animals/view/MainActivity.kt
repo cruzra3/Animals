0b6abc8a7736575d5540f452db0e383345bf4072
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-11-03 10:49:15 -0400

    Add Navigation Configuration using fragments and nav host.

--- app/src/main/java/com/example/android/animals/view/MainActivity.kt
+++ app/src/main/java/com/example/android/animals/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.android.animals
+package com.example.android.animals.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.animals.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
